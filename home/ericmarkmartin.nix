20531f4377207ede081bc9dbcf6703bd43a61b2a
Eric Mark Martin <eric@emm.dev>
2026-04-30 04:39:27 +0000

    default ssh config

--- home/ericmarkmartin.nix
+++ home/ericmarkmartin.nix
@@ -30,11 +30,27 @@
 
 	programs.ssh = {
 		enable = true;
+		enableDefaultConfig = false;
+
 		matchBlocks = {
 			"github.com" = {
 				identityFile = "~/.ssh/id_github";
 				extraOptions.AddKeysToAgent = "yes";
 			};
+			"*" = {
+				forwardAgent = false;
+				serverAliveInterval = 0;
+				serverAliveCountMax = 3;
+				compression = false;
+				addKeysToAgent = "no";
+				hashKnownHosts = false;
+				userKnownHostsFile = "~/.ssh/known_hosts";
+				controlMaster = "no";
+				controlPath = "~/.ssh/master-%r@%n:%p";
+				controlPersist = "no";
+			};
 		};
+
+
 	};
 }
