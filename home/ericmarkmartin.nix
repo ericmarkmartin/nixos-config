953ad16e9f17b1b2ed7c096ccd68b9dbfe377d3e
Eric Mark Martin <eric@emm.dev>
2026-04-30 04:50:24 +0000

    startship and reformat

--- home/ericmarkmartin.nix
+++ home/ericmarkmartin.nix
@@ -1,56 +1,86 @@
 { pkgs, ... }: {
-	home.username = "ericmarkmartin";
-	home.homeDirectory = "/home/ericmarkmartin";
-	home.stateVersion = "25.11";
+    home.username = "ericmarkmartin";
+    home.homeDirectory = "/home/ericmarkmartin";
+    home.stateVersion = "25.11";
 
-	home.packages = with pkgs; [
-		ripgrep fd bat eza fzf jq neovim gh
-	];
+    home.packages = with pkgs; [
+        ripgrep fd bat eza fzf jq neovim gh starship
+    ];
 
-	programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
 
-	programs.git = {
-		enable = true;
-		settings = {
-			user = {
-				name = "Eric Mark Martin";
-				email = "eric@emm.dev";
-			};
-		};
-	};
+    programs.git = {
+        enable = true;
+        settings = {
+            user = {
+                name = "Eric Mark Martin";
+                email = "eric@emm.dev";
+            };
+        };
+    };
 
-	programs.zsh = {
-		enable = true;
-	};
+    programs.zsh = {
+        enable = true;
+    };
 
-	programs.direnv = {
-		enable = true;
-		nix-direnv.enable = true;
-	};
+    programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+    };
 
-	programs.ssh = {
-		enable = true;
-		enableDefaultConfig = false;
+    programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
 
-		matchBlocks = {
-			"github.com" = {
-				identityFile = "~/.ssh/id_github";
-				extraOptions.AddKeysToAgent = "yes";
-			};
-			"*" = {
-				forwardAgent = false;
-				serverAliveInterval = 0;
-				serverAliveCountMax = 3;
-				compression = false;
-				addKeysToAgent = "no";
-				hashKnownHosts = false;
-				userKnownHostsFile = "~/.ssh/known_hosts";
-				controlMaster = "no";
-				controlPath = "~/.ssh/master-%r@%n:%p";
-				controlPersist = "no";
-			};
-		};
+        matchBlocks = {
+            "github.com" = {
+                identityFile = "~/.ssh/id_github";
+                extraOptions.AddKeysToAgent = "yes";
+            };
+            "*" = {
+                forwardAgent = false;
+                serverAliveInterval = 0;
+                serverAliveCountMax = 3;
+                compression = false;
+                addKeysToAgent = "no";
+                hashKnownHosts = false;
+                userKnownHostsFile = "~/.ssh/known_hosts";
+                controlMaster = "no";
+                controlPath = "~/.ssh/master-%r@%n:%p";
+                controlPersist = "no";
+            };
+        };
 
 
-	};
+    };
+
+    programs.starship = {
+        enable = true;
+        settings = {
+            add_newline = false;
+            format = "$directory$git_branch$git_status$character";
+
+            character = {
+        	success_symbol = "[>](bold green)";
+        	error_symbol = "[>](bold red)";
+            };
+
+            directory = {
+        	truncation_length = 3;
+        	truncate_to_repo = true;
+            };
+
+            git_branch = {
+        	symbol = "";
+        	style = "bold purple";
+            };
+
+            git_status = {
+        	style = "bold yellow";
+            };
+
+            aws.disabled = true;
+            gcloud.disabled = true;
+        };
+    };
 }
